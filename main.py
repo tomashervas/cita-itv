c32edba808a49c2a758cb4e2b489c0f6b305d648
tomas <josetomashervas@gmail.com>
2024-04-30 14:50:23 +0200

    send mail of results

--- main.py
+++ main.py
@@ -5,66 +5,83 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import time
-import json
-import csv
+import os
+from dotenv import load_dotenv
+from send_mail import send_mail
 
-options = webdriver.FirefoxOptions()
-# options.add_argument("--headless")
-driver = webdriver.Firefox(options=options)
+load_dotenv()
 
-driver.get("https://sitval.com/#/cita_previa")
-WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "/html/body/app-root/itv-alertas/p-dialog/div/div/div[4]/p-footer/button"))
-).click()
+localidades_to_check = ["València (Campanar)", "Catarroja", "Riba-roja de Túria"]
+localidades_disponibles = []
 
-headers = driver.find_elements(By.CSS_SELECTOR, "span.p-tabview-title")
-header_valencia = None
+def hay_citas(municipio):
+    options = webdriver.FirefoxOptions()
+    # options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options)
 
-for header in headers:
-    if header.text == "València":
-        header_valencia = header
-        break
+    driver.get("https://sitval.com/#/cita_previa")
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "/html/body/app-root/itv-alertas/p-dialog/div/div/div[4]/p-footer/button"))
+    ).click()
 
-header_valencia.click()
+    headers = driver.find_elements(By.CSS_SELECTOR, "span.p-tabview-title")
+    header_valencia = None
 
-time.sleep(1)
+    for header in headers:
+        if header.text == "València":
+            header_valencia = header
+            break
 
-localidadesList = driver.find_element(By.XPATH, "/html/body/app-root/div/div/div/itv-cita-previa/div/div/div[2]/itv-select-localidad/div/div/p-tabview/div/div[2]/p-tabpanel[3]/div/div[1]")
+    header_valencia.click()
 
-localidades = localidadesList.find_elements(By.CSS_SELECTOR, "button.localidad")
+    time.sleep(1)
 
-for localidad in localidades:
-    if localidad.text == "Riba-roja de Túria":
-        localidad.click()
-        time.sleep(1)
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "input.form-control"))
-        ).send_keys("1234GPT")
-        time.sleep(1)
-        driver.find_element(By.CSS_SELECTOR, "#topDiv > div > div:nth-child(2) > itv-select-vehiculo > div.row.text-center > div > div:nth-child(1) > div > span.desc-vehiculo").click()
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "/html/body/app-root/div/div/div/itv-cita-previa/p-dialog[1]/div/div/div[3]/div/div[1]/div"))
-        ).click()
-        time.sleep(1)
-        listaMeses = driver.find_element(By.CSS_SELECTOR, "#topDiv > div > div:nth-child(2) > itv-select-cita > div > div > div:nth-child(1) > div > ul")
-        meses = listaMeses.find_elements(By.TAG_NAME, "a")
-        for mes in meses:
-            mes.click()
-            nohaycita = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "/html/body/app-root/div/div/div/itv-cita-previa/div/div/div[2]/itv-select-cita/div/div/div[2]/div/div[1]"))
-            ) 
-            if nohaycita.text.strip() == "SIN TURNOS DISPONIBLES":
-                print(f"No hay citas en {mes.text}")
-            else:
-                print(f"Hay citas en {mes.text}")
+    localidadesList = driver.find_element(By.XPATH, "/html/body/app-root/div/div/div/itv-cita-previa/div/div/div[2]/itv-select-localidad/div/div/p-tabview/div/div[2]/p-tabpanel[3]/div/div[1]")
+
+    localidades = localidadesList.find_elements(By.CSS_SELECTOR, "button.localidad")
+
+    # for localidad in localidades:
+    #     print(localidad.text)
+
+    for localidad in localidades:
+        localidad_nombre = localidad.text
+        if localidad.text == municipio:
+            print(localidad.text)
+            localidad.click()
+            time.sleep(1)
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "input.form-control"))
+            ).send_keys("1234GPT")
+            time.sleep(1)
+            driver.find_element(By.CSS_SELECTOR, "#topDiv > div > div:nth-child(2) > itv-select-vehiculo > div.row.text-center > div > div:nth-child(1) > div > span.desc-vehiculo").click()
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "/html/body/app-root/div/div/div/itv-cita-previa/p-dialog[1]/div/div/div[3]/div/div[1]/div"))
+            ).click()
+            time.sleep(1)
+            listaMeses = driver.find_element(By.CSS_SELECTOR, "#topDiv > div > div:nth-child(2) > itv-select-cita > div > div > div:nth-child(1) > div > ul")
+            meses = listaMeses.find_elements(By.TAG_NAME, "a")
+            for mes in meses:
+                mes.click()
+                nohaycita = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, "/html/body/app-root/div/div/div/itv-cita-previa/div/div/div[2]/itv-select-cita/div/div/div[2]/div/div[1]"))
+                )
+                if nohaycita.text.strip() == "SIN TURNOS DISPONIBLES":
+                    print(f"No hay citas en {mes.text} en {localidad_nombre}")
+                else:
+                    print(f"Hay citas en {mes.text} en {localidad_nombre}")
+                    localidades_disponibles.append("Hay citas en " + mes.text + " en " + localidad_nombre)
                 
-            
-        time.sleep(2)
-        break
+            break
 
+    time.sleep(1)
+    driver.quit()
 
+for localidad in localidades_to_check:
+    hay_citas(localidad)
 
+print('-------------------Resultado:-------------------')
+for localidad in localidades_disponibles:
+    print(localidad)
 
-
-time.sleep(5)
-driver.quit()
+if len(localidades_disponibles) > 0:
+    send_mail(os.getenv("SENDER_EMAIL"), os.getenv("PASSMAIL"), os.getenv("RECEIVER_EMAIL"), "Citas disponibles", localidades_disponibles)
---
+++ send_mail.py
@@ -0,0 +1,59 @@
+import os
+import logging
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+
+   
+def send_mail(sender, password, receiver, subject, dates):
+    """
+    Sends an email using the provided sender, password, receiver, subject, and body.
+    
+    Args:
+        sender (str): The email address of the sender.
+        password (str): The password associated with the sender's email address.
+        receiver (str): The email address of the receiver.
+        subject (str): The subject line of the email.
+        dates (str[]): A list of dates to be included in the email body.
+        
+    """ 
+
+
+    sender_email = sender
+    sender_password = password
+    receiver_email = receiver
+
+    em = MIMEMultipart()
+
+    em['From'] = sender_email
+    em['To'] = receiver_email
+    em['Subject'] = subject
+
+    mail_body = f"""
+                    <html>
+                        <body style="margin:0;padding:0;">
+                            <p style="font-size: large;">Hay citas disponibles!</p>"""
+                            
+    for date in dates:
+        mail_body += f'p style="font-weight: bold;">{date}</p>'
+        
+    mail_body += """     
+                        </body>
+                    </html>
+                """
+
+    em.attach(MIMEText(mail_body, 'html'))
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, receiver_email, em.as_string())
+        logging.info(f"Correo electrónico enviado correctamente a {receiver_email}")
+        print(f"Correo electrónico enviado correctamente a {receiver_email}")
+    except Exception as e:
+        logging.info(f"No se pudo enviar el correo: {str(e)}")
+        print(f"No se pudo enviar el correo: {str(e)}")
+        quit()
+
+    server.quit()
